a7dd8c2007335201ff78305d1b9754a6ee6b8f82
Max <22273800+NotThatBowser@users.noreply.github.com>
2020-01-13 17:27:57 +0000

    Improve hand animation timing.

--- circle_clock/lib/config/config_hand_data.dart
+++ circle_clock/lib/config/config_hand_data.dart
@@ -2,6 +2,6 @@ import 'package:analog_clock/models/circle_hand_model.dart';
 
 class ConfigHandData {
   static const hourHand = CircleHandModel(1.2, 0.25, 4900);
-  static const minuteHand = const CircleHandModel(0.6, 0.50, 1900);
+  static const minuteHand = const CircleHandModel(0.6, 0.50, 2900);
   static const secondHand = const CircleHandModel(0.1, 0.75, 900);
 }
